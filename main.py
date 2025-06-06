cdff09df4e0d6cf3ac8adabb003efc64146b44c0
jbarneyy <js_barnett@outlook.com>
2025-06-06 14:06:06 -0700

    Added circleshape class and player class, player can turn using a and d keys

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import player
 from constants import *
 
 def main():
@@ -8,7 +9,12 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    PLAYER = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
 
@@ -16,13 +22,22 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # On each game loop: Fill screen obj with black background. Call player obj to draw itself to screen.
+        
+        SCREEN.fill("black")
 
-        screen.fill("black")
+        PLAYER.update(dt)
+        PLAYER.draw(SCREEN)
+        
 
 
 
         # Pygame/Display module method for refreshing screen.
         pygame.display.flip()
+
+        # Pauses game from refreshing for 1/60th of a second. dt stores time in seconds from last refresh.
+        dt = game_clock.tick(60) / 1000
+        
 
 
 
